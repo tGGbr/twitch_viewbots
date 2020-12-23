86c326fe06690b001414244286cad538a09aea32
tGG <>
2020-12-23 03:49:54 -0300

    Added functionality to BotManager and Bot classses

--- core/bot.py
+++ core/bot.py
@@ -1,22 +1,54 @@
 """This module implements the bot."""
-from typing import List
+import asyncio
+from typing import List, Optional
 
+from . import http_client as hc
 from . import proxies
 
 
 class BotManager:
     """This class manages several Twitch viewbots."""
-
-    def __init__(self, proxy_strat: proxies.ProxyBase):
+    def __init__(self,
+                 proxy_strat: proxies.ProxyBase,
+                 loop: asyncio.AbstractEventLoop = None):
         """Implement constructor."""
-        self.proxies = proxy_strat.proxy_list
+        self.loop = loop or asyncio.get_event_loop()
+        self.proxy_url_list = proxy_strat.proxy_list
         self.config = proxy_strat.config
         self.bots: List[Bot] = []
+
+    async def main(self):
+        self.bots = [Bot(p_url) for p_url in self.proxy_url_list]
+        await asyncio.gather(*[b.start() for b in self.bots])
+        await asyncio.gather(*[b.close() for b in self.bots])
+
+    def run(self):
+        try:
+            self.loop.run_until_complete(self.main())
+
+        finally:
+            self.loop.close()
 
 
 class Bot:
     """This class implements a Twitch viewbot."""
-
-    def __init__(self):
+    def __init__(self, proxy_url: Optional[str], n_sessions: int = 1):
         """Implement constructor."""
-        self.sessions: List[]
+        self.n_sessions: int = n_sessions
+        self.sessions: List[hc.Http] = []
+
+    async def start(self):
+        """Run when bot starts up."""
+        self.sessions = [hc.Http() for _ in range(self.n_sessions)]
+
+    async def close(self):
+        """Run when bot stops."""
+        await asyncio.gather(*[s.close() for s in self.sessions if s])
+
+
+if __name__ == "__main__":
+    """Basic testing"""
+    p_strat = proxies.ProxyTor()
+    p_strat.proxy_list = [None] * 10
+    manager = BotManager(proxy_strat=p_strat)
+    manager.run()
--- core/proxies.py
+++ core/proxies.py
@@ -4,16 +4,14 @@ from typing import Dict, List, Optional
 
 class ProxyBase:
     """This is the base class for all proxy classes."""
-
     def __init__(self):
         """Implement constructor."""
-        self.proxy_list: List[str] = []
+        self.proxy_list: List[Optional[str]] = []
         self.config: Dict[str, Optional[str, int]] = {}
 
 
 class ProxyTor(ProxyBase):
     """This class implements proxies from Tor."""
-
     def __init__(self):
         """Implement constructor."""
         super().__init__()
@@ -21,7 +19,6 @@ class ProxyTor(ProxyBase):
 
 class ProxyFile:
     """This class implements proxies from a file."""
-
     def __init__(self):
         """Implement constructor."""
         super().__init__()
@@ -29,7 +26,6 @@ class ProxyFile:
 
 class ProxyNordVPN:
     """This class implements proxies from NordVPN accounts."""
-
     def __init__(self):
         """Implement constructor."""
         super().__init__()
